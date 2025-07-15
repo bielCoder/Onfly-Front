3ba4b0d9e17ef4cd03ddcf1a91c7cf6cf37ed627
bielCoder <gabrielhenrique9253@gmail.com>
2025-07-15 03:50:18 -0300

    Feat: Terminei ajuste e validações de erros

--- src/App.vue
+++ src/App.vue
@@ -23,5 +23,27 @@ html, body {
   box-sizing: border-box;
 }
 
+/* Largura do scroll */
+::-webkit-scrollbar {
+    width: 8px;  /* Para scroll vertical */
+    height: 8px; /* Para scroll horizontal */
+}
+
+/* Cor de fundo da barra de rolagem (trilho) */
+::-webkit-scrollbar-track {
+    background: #1c1c1c; /* Fundo escuro ou cor do layout */
+    border-radius: 0px 4px 4px 0px;
+}
+
+/* Parte que se move (thumb) */
+::-webkit-scrollbar-thumb {
+    background: #1449c3e6; /* Cor da "bolinha" do scroll */
+    border-radius: 0px 4px 4px 0px;
+}
+
+/* Ao passar o mouse no thumb */
+::-webkit-scrollbar-thumb:hover {
+    background: #1449c3e6; /* Cor no hover */
+}
 
 </style>
